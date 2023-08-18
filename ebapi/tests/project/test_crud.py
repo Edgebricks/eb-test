60c0961dd0216ac9b81672c57e2951aa887399c0
ankitjain-edgebricks <99635493+ankitjain-edgebricks@users.noreply.github.com>
2023-08-18 07:20:55 +0530

    [qa-191]: added the project crud operations (#5)

--- ebapi/tests/project/test_crud.py
+++ ebapi/tests/project/test_crud.py
@@ -1,35 +1,48 @@
 #! /usr/bin/python
 #
 # Author: ankit@edgebricks.com
-# (c) 2022 Edgebricks Inc
+# Copyright (c) 2021-2023 Edgebricks Inc.
+
+import pytest
 
 from ebapi.common.config import ConfigParser
-
-# from ebapi.lib.keystone import Users, Roles
 from ebapi.lib.edgebricks import BUs, Projects
 
 
 class TestProjectCRUD:
     testConfig = ConfigParser()
+    # initialise bu
+    buObj = BUs()
+    domainName = testConfig.getDomainName()
+    userName = testConfig.getProjectAdmin()
+    userPwd = testConfig.getProjectAdminPassword()
 
-    def test_project_crud_001(cls):
+    @classmethod
+    def setup_class(cls):
         # create BU
-        buObj = BUs()
-        domainName = cls.testConfig.getDomainName()
-        userName = cls.testConfig.getProjectAdmin()
-        userPwd = cls.testConfig.getProjectAdminPassword()
-        buID = buObj.create(buName=domainName, userName=userName, userPwd=userPwd)
-        assert buID
+        cls.buID = cls.buObj.create(
+            buName=cls.domainName, userName=cls.userName, userPwd=cls.userPwd
+        )
+        assert cls.buID
 
         # get bu
-        buResp = buObj.get(buID)
-        assert buResp["name"] == domainName
+        buResp = cls.buObj.get(cls.buID)
+        assert buResp["name"] == cls.domainName
 
         # wait for bu to be created
-        assert buObj.waitForState(buID, state=BUs.BU_STATE_CREATED)
+        assert cls.buObj.waitForState(cls.buID, state=BUs.BU_STATE_CREATED)
 
-        # create Project in the above bu
-        projObj = Projects(domainName, userName, userPwd)
+    @classmethod
+    def teardown_class(cls):
+        # delete bu
+        assert cls.buObj.delete(cls.buID, force_delete="true")
+
+        # wait for bu to be deleted
+        assert cls.buObj.waitForState(cls.buID, state=BUs.BU_STATE_DELETED)
+
+    def test_project_crud_001(cls):
+        # create Project in the above created bu
+        projObj = Projects(cls.domainName, cls.userName, cls.userPwd)
         metadata = {"templateId": "Large", "custom_template": "true"}
         compQuota = {
             "cores": 128,
@@ -58,29 +71,83 @@ class TestProjectCRUD:
         }
 
         projectName = cls.testConfig.getProjectName()
-        projID = projObj.create(
-            projectName, buID, metadata, compQuota, strQuota, netQuota
-        )
-        assert projID
+        try:
+            projID = projObj.create(
+                projectName, cls.buID, metadata, compQuota, strQuota, netQuota
+            )
+            assert projID
 
-        # get project
-        projResp = projObj.get(projID)
-        assert projResp["name"] == projectName
+            # get project
+            projResp = projObj.get(projID)
+            assert projResp["name"] == projectName
 
-        # wait for project to be created
-        assert projObj.waitForState(projID, state=Projects.PROJ_STATE_CREATED)
+            # wait for project to be created
+            assert projObj.waitForState(projID, state=Projects.PROJ_STATE_CREATED)
 
-        # delete project
-        assert projObj.delete(projID)
+            # update project description
+            newDesc = "ebtestProject description updated"
+            updatedProjectResp = projObj.update(
+                cls.buID, projID, projectName, desc=newDesc
+            )
+            assert updatedProjectResp["description"] == newDesc
 
-        # wait for project to be deleted
-        assert projObj.waitForState(projID, state=Projects.PROJ_STATE_DELETED)
+        finally:
+            # delete project
+            assert projObj.delete(projID, force_delete=True)
 
-        # delete bu
-        assert buObj.delete(buID)
+            # wait for project to be deleted
+            assert projObj.waitForState(projID, state=Projects.PROJ_STATE_DELETED)
 
-        # wait for bu to be deleted
-        assert buObj.waitForState(buID, state=BUs.BU_STATE_DELETED)
+    @pytest.mark.parametrize(
+        "projectNames",
+        ["ebtestProjectNew01", "ebtestProjectNew02", "ebtestProjectNew03"],
+    )
+    def test_project_crud_002(cls, projectNames):
+        # create Project in the above created bu
+        projObj = Projects(cls.domainName, cls.userName, cls.userPwd)
+        metadata = {"templateId": "Large", "custom_template": "true"}
+        compQuota = {
+            "cores": 128,
+            "injected_file_content_bytes": -1,
+            "injected_file_path_bytes": -1,
+            "injected_files": -1,
+            "instances": 64,
+            "key_pairs": -1,
+            "metadata_items": -1,
+            "ram": 262144,
+        }
+        strQuota = {
+            "snapshots": 640,
+            "backup_gigabytes": -1,
+            "backups": -1,
+            "volumes": 640,
+            "gigabytes": 25600,
+        }
+        netQuota = {
+            "router": 30,
+            "subnet": -1,
+            "network": 30,
+            "port": -1,
+            "floatingip": 64,
+            "pool": -1,
+        }
 
-    def test_project_crud_002(cls):
-        assert True
+        try:
+            projID = projObj.create(
+                projectNames, cls.buID, metadata, compQuota, strQuota, netQuota
+            )
+            assert projID
+
+            # get project
+            projResp = projObj.get(projID)
+            assert projResp["name"] == projectNames
+
+            # wait for project to be created
+            assert projObj.waitForState(projID, state=Projects.PROJ_STATE_CREATED)
+
+        finally:
+            # delete project
+            assert projObj.delete(projID, force_delete=True)
+
+            # wait for project to be deleted
+            assert projObj.waitForState(projID, state=Projects.PROJ_STATE_DELETED)
